f9cda5dbf811e8d79b443c6f25198c7e0661cdb3
nachofilippi <nachofilippi@live.com.ar>
2018-04-25 11:30:32 -0300

    Items agregados y borrar(falta terminar)

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Lista, ListaItem } from '../../app/clases/index';
+
 
 @Component({
   selector: 'app-agregar',
@@ -7,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class AgregarComponent implements OnInit {
 
     nombreLista: string;
-    nombreItem: string;
+    nombreItem: string ="";
+
+    items:ListaItem[] = [];
 
   constructor() { }
 
   ngOnInit() { }
+
+  agregar(){
+      if(this.nombreItem.length == 0){
+          return;
+      }
+
+      let item = new ListaItem();
+      item.nombre = this.nombreItem;
+
+      this.items.push(item);
+      this.nombreItem= "";
+  }
 
 }
