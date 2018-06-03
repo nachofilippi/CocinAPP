bd61caf75184027ee7bfbc8ae0e940a371b2a1b6
nlopezm <nlopezmarini@gmail.com>
2018-06-03 20:33:01 -0300

    imagenes

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController, NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 import { ListaRecetasServicios } from '../../app/servicios/lista-recetas';
 import {RestProvider} from '../../providers/rest/rest';
@@ -10,24 +11,25 @@ import {RestProvider} from '../../providers/rest/rest';
 })
 export class AgregarComponent implements OnInit {
 
-  receta: any;
-  categorias:any;
-  ingredientes: any;
-  ingredientesElegidos: any;
+    receta: any;
+    categorias: any;
+    ingredientes: any;
+    ingredientesElegidos: any;
   item: any;
   cantidadItem: number;
 
   constructor(public alertCtrl: AlertController,
     public navCtrl: NavController,
     public _listaRecetas: ListaRecetasServicios,
-    public rest: RestProvider
+    public rest: RestProvider,
+    private camera: Camera
   ) { }
 
   ngOnInit() { 
       this.receta={};
       this.receta.ingredientes=[];
       this.receta.pasos=[];
-      this.receta.imagenes=["http://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2017/07/cortesdecarne.jpg"];
+      this.receta.imagenes=[];
       this.ingredientesElegidos=[];
       this.rest.getCategoriasRecetas().subscribe(data => {this.categorias = data}, offline => {this.categorias = offline;});
       this.rest.getIngredientes().subscribe(data => {this.ingredientes = data}, offline => {this.ingredientes = offline;});
@@ -47,7 +49,11 @@ export class AgregarComponent implements OnInit {
   }
 
   borrarPaso(i: number) {
-      this.receta.pasos.splice(i, 1);
+      this.receta.pasos.splice(i, 1);    
+  }
+  
+  borrarImagen(i: number) {
+      this.receta.imagenes.splice(i, 1);
   }
   
   borrarItem(i: number) {
@@ -56,11 +62,29 @@ export class AgregarComponent implements OnInit {
   }
 
   postReceta() {
-      this.receta;
       this.ingredientesElegidos.forEach((value)=> {
           this.receta.ingredientes.push ({"ingrediente":value.id,"cantidad":value.cantidad});
       });
       this.rest.postReceta(this.receta).subscribe(data => {console.log (data)}, offline => {console.log(offline)});
       this.navCtrl.pop();
+  }
+  
+  takePhoto(sourceType: number = 0) {
+      //0 para galería - 1 para Cámara
+      const options: CameraOptions = {
+          quality: 50,
+          destinationType: this.camera.DestinationType.DATA_URL,
+          encodingType: this.camera.EncodingType.JPEG,
+          mediaType: this.camera.MediaType.PICTURE,
+          correctOrientation: true,
+          sourceType: sourceType,
+      }
+
+      this.camera.getPicture(options).then((imageData) => {
+          let base64Image = 'data:image/jpeg;base64,' + imageData;
+          this.receta.imagenes.push(base64Image);
+      }, (err) => {
+          // Handle error
+      });
   }
 }
