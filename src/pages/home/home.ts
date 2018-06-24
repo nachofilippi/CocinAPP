45d3c53e14dbbffff1e5d6f00a5bf00c105a4b75
nachofilippi <nachofilippi@live.com.ar>
2018-06-24 17:12:16 -0300

    mejoras en tamano letra y borrado de ingredientes en home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ListPage } from '../list/list';
 import { AgregarComponent } from '../agregar/agregar.component';
-import { IngredientesPage } from '../ingredientes/ingredientes.component';
+
 
 
 @Component({
@@ -25,11 +25,7 @@ export class HomePage {
     // we wouldn't want the back button to show in this scenario
     this.navCtrl.push(AgregarComponent);
   }
-  openPageIngredientes() {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.navCtrl.push(IngredientesPage);
-  }
+
 
 
 
