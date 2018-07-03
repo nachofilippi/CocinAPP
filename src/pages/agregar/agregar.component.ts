da15550a038745857d1c2c09bfeeec333cc0ce99
nlopezm <nlopezmarini@gmail.com>
2018-07-03 15:13:14 -0300

    Creaciín recetas

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController, NavController } from 'ionic-angular';
+import { AlertController, NavController, ModalController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 
 import { ListaRecetasServicios } from '../../app/servicios/lista-recetas';
 import {RestProvider} from '../../providers/rest/rest';
 import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import {IngredientesPage} from '../ingredientes/ingredientes.component';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'app-agregar',
@@ -31,7 +33,9 @@ export class AgregarComponent implements OnInit {
     public _listaRecetas: ListaRecetasServicios,
     public rest: RestProvider,
     private camera: Camera,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    public modalCtrl: ModalController,
+    public toastCtrl: ToastController
   ) {
     this.recetaForm = this.formBuilder.group({
       nombre: ['', Validators.required],
@@ -39,7 +43,7 @@ export class AgregarComponent implements OnInit {
       tiempo: ['', Validators.compose([Validators.required, Validators.min(1), Validators.max(180)])],
       video: [''],
       categoria: ['', Validators.required],
-      dificultad: ['', Validators.required]
+      dificultad: [0, Validators.required]
     });
   }
 
@@ -83,23 +87,43 @@ export class AgregarComponent implements OnInit {
       this.receta.imagenes.splice(i, 1);
   }
 
-  borrarItem(i: number) {
-      this.ingredientes.push(this.ingredientesElegidos[i]);
+  borrarItem(i: number, id: number) {
+    this.ingredientes.forEach(element => {
+      if (element.id === id)
+        element.elegido = false;
+    });
       this.ingredientesElegidos.splice(i, 1);
   }
 
   postReceta() {
-      this.ingredientesElegidos.forEach((value)=> {
-          this.receta.ingredientes.push ({"ingrediente":value.id,"cantidad":value.cantidad});
-      });
-      this.receta.nombre=this.recetaForm.value.nombre;
-      this.receta.categoria=this.recetaForm.value.categoria;
-      this.receta.dificultad=this.recetaForm.value.dificultad;
-      this.receta.personas=this.recetaForm.value.personas;
-      this.receta.tiempo=this.recetaForm.value.tiempo;
-      this.receta.video=this.recetaForm.value.video;
-      this.rest.postReceta(this.receta).subscribe(data => {}, offline => {});
-      this.navCtrl.pop();
+    this.ingredientesElegidos.forEach((value) => {
+      if (value.cantidad && value.cantidad > 1)
+        this.receta.ingredientes.push({ "ingrediente": value.id, "cantidad": Math.floor(value.cantidad) });
+    });
+    this.receta.nombre = this.recetaForm.value.nombre;
+    this.receta.categoria = this.recetaForm.value.categoria;
+    this.receta.dificultad = this.recetaForm.value.dificultad;
+    this.receta.personas = this.recetaForm.value.personas;
+    this.receta.tiempo = this.recetaForm.value.tiempo;
+    this.receta.video = this.recetaForm.value.video;
+    this.rest.postReceta(this.receta).subscribe(data => { }, offline => { });
+    let toast = this.toastCtrl.create({
+      message: "Tu receta creada correctamente! Muchas gracias por colaborar con CocinApp.",
+      duration: 7000,
+      position: 'bottom',
+      showCloseButton: true,
+      closeButtonText: "Cerrar"
+    });
+    toast.present();
+    this.navCtrl.pop();
+  }
+
+  validar(){
+    let array:any=[];
+    array=this.ingredientesElegidos.filter(ing=> {
+     return !ing.cantidad || ing.cantidad < 1;
+    });
+    return !array.length;
   }
 
   takePhoto(sourceType: number = 0) {
@@ -119,5 +143,39 @@ export class AgregarComponent implements OnInit {
       }, (err) => {
           // Handle error
       });
+  }
+
+  seleccionarIngredientes() {
+    let ingredientes = this.ingredientes;
+    let modal = this.modalCtrl.create(IngredientesPage, { ingredientes }, { cssClass: 'filtros-modal' });
+    modal.onDidDismiss(data => {
+      if (data) {
+        this.ingredientes = data;
+        this.ingredientesElegidos = data.filter((ingrediente) => {
+          return (ingrediente.elegido);
+        });
+      }
+    });
+    modal.present();
+  }
+
+  crearReceta() {
+    let alert = this.alertCtrl.create({
+      title: 'Crear receta "' + this.recetaForm.value.nombre + '"',
+      message: '¿Estás seguro que deseás crear esta receta?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            this.postReceta();
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
