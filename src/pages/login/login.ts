ff446166d2d525276c03dc2d9d0e6c46df1e7a7f
nlopezm <nlopezmarini@gmail.com>
2018-07-04 12:28:23 -0300

    Login presentacion

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -15,6 +15,31 @@ export class LoginPage {
 
   constructor(private login: LoginProvider, public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
     this.usuario = this.navParams.get("usr");
+    switch (this.usuario.email) {
+      case 'grupo_okfkqtr_mascotas@tfbnw.net': {
+        this.usuario.foto = 'https://www.sura.com/blogs/images1/tener-mascotas-bueno-salud-mental-emocional.jpg';
+        break;
+      }
+      case 'grupo_mnpwchq_paquetes@tfbnw.net': {
+        this.usuario.foto = 'http://spainbox.com/wp-content/uploads/2013/05/enviar-paquetes-internacionales.jpg';
+        break;
+      }
+      case 'grupo_zetwthb_modelos@tfbnw.net': {
+        this.usuario.foto = 'http://modoeficaz.com/wp-content/uploads/2015/07/My-Pictours-comparador-de-fotografos.jpg';
+        break;
+      }
+      case 'alicia_efoxfev_mogliani@tfbnw.net': {
+        this.usuario.foto = 'https://www.webcampus.uade.edu.ar/Fotos/7/ID_79077.jpg';
+        break;
+      }
+      case 'nahuel.lopez.1997@gmail.com': {
+        break;
+      }
+      default: {
+        this.usuario.foto = '././assets/imgs/fork.png';
+        break;
+      }
+    }
   }
 
   logout() {
